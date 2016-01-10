deba59114bcb28ce1e1663c668b80981513af580
codethejason <ihaveapet1@hotmail.com>
2016-01-09 23:17:11 -0500

    POST request to github for token **not working

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //import modules
 var http = require('http');
 var dispatcher = require('httpdispatcher');
+var request = require('request');
 
 
 var state = Math.round(Math.random()*10);
 
 var options = {
   clientID: '7d71da50c080b8899fa5',
+  secret: '',
   scope: '',
   redirectURI: 'http://37.187.42.14:8080/callback'
 };
@@ -31,14 +33,35 @@ dispatcher.onGet("/callback", function(req, res) {
   var url = require('url');
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  if(state) { //supposed to be if states match
+  //returns something like { code: '6cd032d64f7b45f0d339', state: '10' }
+  if(query.state == 10) { //supposed to be if states match
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(query);
+    requestToken(query.code);
   }
   else {
     res.end('Sorry, an error occured.');
   }
 });
+
+dispatcher.onGet("/dashboard", function(req, res) {
+  res.end("Hello, you made it!");
+}
+
+                 
+var requestToken = function (code) {
+  var arguments = {
+    code: code,
+    client_id: options.clientID,
+    client_secret: options.secret,
+    redirect_uri: 'http://37.187.42.14:8080/oauth/dashboard',
+    state: state
+  };
+  request.post('https://github.com/login/oauth/access_token', formData: arguments, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.end(body);
+    }
+})
+};
 
 //define port for listening for web server
 const PORT = 8080;
